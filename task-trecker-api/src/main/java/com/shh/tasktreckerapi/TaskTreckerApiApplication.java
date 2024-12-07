c516d1c464962a7f925c6eb7ee5140bc49298ec0
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-12-07 10:39:19 +0300

    Created basic project

---
+++ task-trecker-api/src/main/java/com/shh/tasktreckerapi/TaskTreckerApiApplication.java
@@ -0,0 +1,13 @@
+package com.shh.tasktreckerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTreckerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTreckerApiApplication.class, args);
+    }
+
+}
